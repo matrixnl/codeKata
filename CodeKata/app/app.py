5590a582f4e09cacad15611de8bc309dda3030ac
Mahesh <hesh.here@gmail.com>
2020-11-17 08:43:42 +1100

    app with io

--- CodeKata/app/app.py
+++ CodeKata/app/app.py
@@ -1,11 +1,72 @@
+from pyspark.sql.functions import concat, format_string
+from pyspark.sql.session import SparkSession
+from pyspark.context import SparkContext
+import numpy as np
+import pandas as pd
 from flask import Flask
+import csv
 import os
+import sys
 app = Flask('codeKata')
 
 
 @app.route("/")
 def index():
-    return true
+    try:
+        sc = SparkContext.getOrCreate()
+        spark = SparkSession(sc)
+
+        try:
+            schemaFile = spark.read.json('../input/spec.json', multiLine=True)
+        except OSError as err:
+            print("OS error: {0}".format(err))
+
+        fixedWidthFile = "../input/fixedWidth.txt"
+        outputCsv = '../output/output.csv'
+        outputTxt = "../output/output.txt"
+
+        data = np.random.randint(1, 10, size=(5, 10))
+        pysdf = spark.createDataFrame(pd.DataFrame(columns=schemaFile.collect()[
+            0][0], data=np.array(data.astype('U'))))
+        isFwf = generateFixedWidthFile(outputTxt, pysdf, schemaFile)
+        isDlm = generateDelimitedFile(outputCsv, fixedWidthFile, schemaFile)
+    finally:
+        sc.stop()
+        return isFwf + ' and ' + isDlm
+
+
+# Generate a fixed width file using the provided specifications.
+def generateFixedWidthFile(outputTxt, pysdf, schemaFile):
+    try:
+        fixedWidth = schemaFile.collect()[0][4]
+        ljust = []
+        for w in fixedWidth:
+            ljust.append(r"%-{width}s".format(width=w))
+        pysdf.select(concat(*[format_string(l, c)
+                              for c, l in zip(pysdf.columns, ljust)])).show(truncate=False)
+        pysdf.coalesce(1).write.option("header", schemaFile.collect()[0][3]).option(
+            "encoding", schemaFile.collect()[0][2]).mode("append").save(outputTxt)
+    except Exception as ex:
+        print("Exception: {0}".format(ex))
+        return "Unable to generate fixed width file"
+    return "Fixed width file generated"
+
+
+# Parser that can parse the fixed width file and generate a delimited file.
+def generateDelimitedFile(outputCsv, fixedWidthFile, schemaFile):
+    try:
+        rf = open(fixedWidthFile)
+        wf = open(outputCsv, 'w', encoding=schemaFile.collect()[0][1])
+        writer = csv.writer(wf)
+        for row in rf.readlines():
+            writer.writerow(row.split())
+        rf.close()
+        wf.close()
+    except OSError as err:
+        print("OS error: {0}".format(err))
+        return "Unable to generate delimited file"
+    return "Delimited file generated"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# py-task
+# Code Kata - Problem 1
+
+## Parse fixed width file
+
+- Generate a fixed width file using the provided spec (offset provided in the spec file represent the length of each field).
+- Implement a parser that can parse the fixed width file and generate a delimited file, like CSV for example.
+- DO NOT use python libraries like pandas for parsing. You can use the standard library to write out a csv file (If you feel like)
+- Language choices (Python or Scala)
+- Deliver source via github or bitbucket
+- Bonus points if you deliver a docker container (Dockerfile) that can be used to run the code (too lazy to install stuff that you might use)
+- Pay attention to encoding
+
+## Setup
+
+Clone the repo
+
+```bash
+https://github.com/matrixnl/codeKata.git
+```
+
+Run the server
+
+```bash
+docker-compose up --build
+```
+
+Run the tests
+
+```bash
+python -m pytest
+```
+
+The server will be up on [http://localhost:5000](http://localhost:5000).
+
+## Requirements
+
+Python >= 3.6
+
+## License
+
+[MIT](http://www.opensource.org/licenses/mit-license.html)
